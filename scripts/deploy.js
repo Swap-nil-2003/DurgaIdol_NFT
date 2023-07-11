b842690c24c743b53aca5444f0fd9d8170d13f0a
Swapnil Aichbhaumik <swapnil.aichbhaumik.cse25@heritageit.edu.in>
2023-07-11 12:43:09 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,6 +12,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-  //0x288d236B902E226578c617516c82dECDf74B106E
-
-  //1st hash: 0x90d8a7366e393dfe7deb08972993fbf2f9b2b12225cb68e2d2cd7cb066b5ddd9 
